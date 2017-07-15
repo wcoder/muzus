b664673806276c0e11bc38111651bb1f9d6b9318
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-07-15 20:42:54 +0300

    Add gulp-uglify. Close #2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
 	return gulp.src('./src/*.scss')
@@ -11,4 +13,11 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
 	gulp.watch('./src/*.scss', ['sass']);
+});
+
+gulp.task('build:js', function () {
+	return gulp.src('src/*.js')
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
 });
