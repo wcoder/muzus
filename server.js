659e96d5619535e196cc4535addd63652e25d572
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-08-24 09:15:48 +0300

    Update docs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 app.use('/', express.static('example'));
 app.use('/dist', express.static('dist'));
 app.use('/src', express.static('src'));
+app.use('/docs', express.static('docs'));
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
