9abd13f1c7f10e7c31a19561dd26cdf793e5d93f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2021-07-23 00:11:00 +0300

    Update dev deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var replace = require('gulp-replace');
